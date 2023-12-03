629a3e5bb7a5dc4fa1fe457a44030ec82a4da45f
SteveMustanski <steve.mustanski@gmail.com>
2023-12-03 11:25:56 -0500

    added env params

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // middleware
-
 app.use(express.json());
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// basic example for how to serve static
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from custom middleware');
